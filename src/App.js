7471ace7be36989f1b18edfd14a7285cc3d56ff0
Ilyass <iklassgames@gmail.com>
2025-10-04 22:37:04 +0100

    Styling

--- src/App.js
+++ src/App.js
@@ -1,59 +1,79 @@
 import React from 'react';
 import HTMLFlipBook from 'react-pageflip';
 import './App.css';
+import profile from './assets/profile.jpeg';
 
 function App() {
-  
-  const logo = "";
-  const profile = "";
 
   return (
     <div className="App">
-      <nav>
-        <ul>
-          <li><img src={logo} alt="Logo" /></li>
-          <li><a href="#home-section">Home</a></li>
-          <li><a href="#about-section">About</a></li>
-          <li><a href="#projects-section">Projects</a></li>
-          <li><a href="#contact-section">Contact</a></li>
-        </ul>
-      </nav>
       <div className="content">
-        <HTMLFlipBook width={600} height={450} size='stretch'>
-          <div className='empty-page'></div>
-          <div className='cover'>
-            <h1>Portfolio</h1>
-            <p>Hi there, welcome to my cozy corner of the internet 🌸</p>
-          </div>
-          <div className='page' id='home-section'>
-            <h1>Home</h1>
-            <div className='home-content'>
-              <img src={profile} alt="Profile" />
-              <div className='summary'>
-                <p>What you'll find here:</p>
+        <h1>Portfolio</h1>
+        <div className='flipbook-container'>
+          <HTMLFlipBook className='flipbook' width={450} height={600}>
+            <div className='empty-page'></div>
+            <div className='cover'>
+              <div className='page-content'>
+                <img src="" alt="Logo" />
+                <p>Hi there, welcome to my cozy corner of the internet 🌸</p>
+              </div>
+            </div>
+            <div className='page' id='home-section'>
+              <div className='page-content'>
+                <h1>Home</h1>
+                <div className='home-content'>
+                  <img className='profile' src={profile} alt="Profile" />
+                  <div className='summary'>
+                    <p>What you'll find here:</p>
+                    <ul>
+                      <li>More about myself</li>
+                      <li>My coding projects</li>
+                      <li>My contact infos</li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='page' id='about-section'>
+              <div className='page-content'>
+                <h1>About</h1>
+                <p>Hi, and welcome to my little notebook 🌸 This is where I keep pieces of my journey — the things I’ve built, the ideas I love, and the stories behind them. Feel free to flip through the pages, discover my projects, and say hello. Best enjoyed with a warm drink and a curious heart ☕📖.</p>
+              </div>
+            </div>
+            <div className='page' id='projects-section'>
+              <div className='page-content'>
+                <h1>Projects</h1>
+                <div className='project-grid'>
+                  <div className='project'>
+                    <h3>Spirited Work</h3>
+                    <img src="" alt="Spirited Work" />
+                    <p>A cozy Spirited Away themed Pomodoro timer!</p>
+                  </div>
+                  <div className='project'>
+                    <h3>Jay's Garden</h3>
+                    <img src="" alt="Garden" />
+                    <p>A garden like drawing album!</p>
+                  </div>
+                  <div className='project'>
+                    <h3>Warm Winds</h3>
+                    <img src="" alt="Warm Winds" />
+                    <p>A weather app, presented by Calcifer!</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className='page' id='contact-section'>
+              <div className='page-content'>
+                <h1>Contact</h1>
+                <p>If you have any questions, concerns, or just want to say hi, don't hesitate to reach out!</p>
                 <ul>
-                  <li>More about myself</li>
-                  <li>My coding projects</li>
-                  <li>My contact infos</li>
+                  <li><i class="fa-brands fa-github fa-2x"></i></li>
                 </ul>
               </div>
             </div>
-          </div>
-          <div className='page' id='about-section'>
-            <h1>About</h1>
-            <p>Hi, and welcome to my little notebook 🌸 This is where I keep pieces of my journey — the things I’ve built, the ideas I love, and the stories behind them. Feel free to flip through the pages, discover my projects, and say hello. Best enjoyed with a warm drink and a curious heart ☕📖.</p>
-          </div>
-          <div className='page' id='projects-section'>
-            <h1>Projects</h1>
-            <p>This is the projects page.</p>
-          </div>
-          <div className='page' id='contact-section'>
-            <h1>Contact</h1>
-            <p>This is the contact page.</p>
-          </div>
-          <div className='cover'></div>
-          <div className='empty-page'></div>
-        </HTMLFlipBook>
+            <div className='cover'></div>
+          </HTMLFlipBook>
+        </div>
       </div>
     </div>
   );
